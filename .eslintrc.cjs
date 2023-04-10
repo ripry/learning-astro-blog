1b224de2e03404192fc8086e57f765ac236ce865
Riku YAMAUCHI <ripry258@gmail.com>
2023-04-10 15:16:11 +0000

    :wrench: Refactor ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,24 @@
 // @ts-check
 
 /** @type {import("@typescript-eslint/parser").ParserOptions} */
-const config = {
+module.exports = {
   // ...
   extends: [
     // ...
-    "plugin:astro/recommended",
+    'plugin:astro/recommended',
   ],
   // ...
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ['*.astro'],
       // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
+      parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {
         // override/add rules settings here, such as:
@@ -27,6 +27,4 @@ const config = {
     },
     // ...
   ],
-};
-
-export default config;
+}
